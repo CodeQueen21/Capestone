06a6cd82b7fd746cbcbf77f83011a0f637e813d8
Raelinda Shoates <redfire809@gmail.com>
2024-04-14 04:02:58 -0400

    added a welcome to home component

--- Capstone/src/App.jsx
+++ Capstone/src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 export default App;
