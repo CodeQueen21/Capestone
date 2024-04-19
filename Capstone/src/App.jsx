487eeba81e8b8cdc59af9de6153d9da4024d605d
Raelinda Shoates <redfire809@gmail.com>
2024-04-19 12:46:03 -0400

    added routes and a navbar along with overall styling

--- Capstone/src/App.jsx
+++ Capstone/src/App.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { fetchFoodItems } from "./ajaxHelpers";
+import { Routes, Route } from "react-router-dom";
+import Navigations from "./components/Navigations";
 import Home from "./components/Home";
 import Menu from "./components/Menu";
+import MenuItem from "./components/MenuItem";
 
 function App() {
   const [foodItems, setFoodItems] = useState([]);
@@ -16,7 +19,14 @@ function App() {
   }, []);
   return (
     <>
-      <Menu foodItems={foodItems} />
+      <Navigations />
+      {/* <Menu foodItems={foodItems} /> */}
+      {/* <Home /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/foodItems" element={<Menu foodItems={foodItems} />} />
+        <Route path="/foodItems/:id" element={<MenuItem />} />
+      </Routes>
     </>
   );
 }
